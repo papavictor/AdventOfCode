4c495ce3e0640164175d31039798125890199368
papavictor <github@patrickvalencia.com>
2021-12-03 01:26:11 -0500

    adding day 3 solution

--- 2021/puzzle.py
+++ 2021/puzzle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from copy import deepcopy
+
 def puzzle_1_1():
     with open("1.txt") as fp:
         data = fp.read().strip().splitlines()
@@ -24,9 +26,11 @@ def puzzle_1_2():
             depths.pop(0)
         if len(depths) == 3:
             sumd = sum(depths)
+            #print(sumd)
             if prev_sumd and sumd > prev_sumd:
                 sumd_inc += 1
             prev_sumd = sumd
+        #print(depths)
     return sumd_inc
 
 def puzzle_2_1():
@@ -61,11 +65,82 @@ def puzzle_2_2():
             aim -= amt
     return (hpos * depth)
 
+def puzzle_3_1():
+    with open("3.txt") as fp:
+        data = fp.read().strip().splitlines()
+    bits = {}
+    for line in data:
+        for c in range(len(line)):
+            if c not in bits:
+                bits[c] = {'0': 0, '1': 0}
+            bits[c][line[c]] += 1
+    maxm = ""
+    minm = ""
+    for b in bits:
+        if bits[b]['0'] > bits[b]['1']:
+            maxm += "0"
+            minm += "1"
+        else:
+            maxm += "1"
+            minm += "0"
+    return int(maxm, 2) * int(minm, 2)
+
+def puzzle_3_2():
+    with open("3.txt") as fp:
+        data = fp.read().strip().splitlines()
+    maxdata = deepcopy(data)
+    mindata = deepcopy(data)
+    while len(maxdata) > 1:
+        for c in range(len(maxdata[0])):
+            newmaxdata = deepcopy(maxdata)
+            bits = {"0": 0, "1": 0}
+            maxm = ""
+            for line in maxdata:
+                bits[line[c]] += 1
+            if bits["0"] > bits["1"]:
+                maxm = "0"
+            else:
+                maxm = "1"
+            for line in maxdata:
+                if not line[c] == maxm:
+                    newmaxdata.remove(line)
+                    if len(newmaxdata) == 1:
+                        maxdata = newmaxdata
+                        break
+            else:
+                maxdata = newmaxdata
+                continue
+            break
+    while len(mindata) > 1:
+        for c in range(len(mindata[0])):
+            newmindata = deepcopy(mindata)
+            bits = {"0": 0, "1": 0}
+            minm = ""
+            for line in mindata:
+                bits[line[c]] += 1
+            if bits["0"] > bits["1"]:
+                minm = "1"
+            else:
+                minm = "0"
+            for line in mindata:
+                if not line[c] == minm:
+                    newmindata.remove(line)
+                    if len(newmindata) == 1:
+                        mindata = newmindata
+                        break
+            else:
+                mindata = newmindata
+                continue
+            break
+    return int(mindata[0], 2) * int(maxdata[0], 2)
+
 def main():
     print(puzzle_1_1())
     print(puzzle_1_2())
     print(puzzle_2_1())
     print(puzzle_2_2())
+    print(puzzle_3_1())
+    print(puzzle_3_2())
 
 if __name__ == '__main__':
     main()

c30d37efb580277e8e4162d1d6beaac022eb75b8
papavictor <github@patrickvalencia.com>
2022-12-01 00:17:37 -0500

    2022 kick off, day 1

---
+++ 2022/puzzle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+def puzzle_1_1():
+    with open("1.txt") as fp:
+        data = fp.read().strip().splitlines()
+    maxsum = 0
+    ssum = 0
+    for s in data:
+        if s == '':
+            if ssum > maxsum:
+                maxsum = ssum
+            ssum = 0
+        else:
+            ssum += int(s)
+    if ssum > maxsum:
+        maxsum = ssum
+    return maxsum
+
+def puzzle_1_2():
+    with open("1.txt") as fp:
+        data = fp.read().strip().splitlines()
+    sums = []
+    ssum = 0
+    for s in data:
+        if s == '':
+            sums.append(ssum)
+            ssum = 0
+        else:
+            ssum += int(s)
+    sums.append(ssum)
+    return sum(sorted(sums)[-3:])
+
+def main():
+    print(puzzle_1_1())
+    print(puzzle_1_2())
+
+if __name__ == '__main__':
+    main()

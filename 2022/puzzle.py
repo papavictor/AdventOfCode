ab1328076aee2ea0a50ebd8711ec991cf08075a9
papavictor <github@patrickvalencia.com>
2022-12-02 00:37:29 -0500

    2022, day 2

--- 2022/puzzle.py
+++ 2022/puzzle.py
@@ -30,9 +30,33 @@ def puzzle_1_2():
     sums.append(ssum)
     return sum(sorted(sums)[-3:])
 
+def puzzle_2_1():
+    with open("2.txt") as fp:
+        data = fp.read().strip().splitlines()
+    scores = {"X": 1, "Y": 2, "Z": 3}
+    total = 0
+    wld = {"A": {"X": 3, "Y": 6, "Z": 0}, "B": {"X": 0, "Y": 3, "Z": 6}, "C": {"X": 6, "Y": 0, "Z": 3}}
+    for line in data:
+        (op, my) = line.split(" ")
+        total += scores[my] + wld[op][my]
+    return total
+
+def puzzle_2_2():
+    with open("2.txt") as fp:
+        data = fp.read().strip().splitlines()
+    shapes = {"A": {"X": 3, "Y": 1, "Z": 2}, "B": {"X": 1, "Y": 2, "Z": 3}, "C": {"X": 2, "Y": 3, "Z": 1}}
+    wld_score = {"X": 0, "Y": 3, "Z": 6}
+    total = 0
+    for line in data:
+        (op, end) = line.split(" ")
+        total += wld_score[end] + shapes[op][end]
+    return total
+
 def main():
     print(puzzle_1_1())
     print(puzzle_1_2())
+    print(puzzle_2_1())
+    print(puzzle_2_2())
 
 if __name__ == '__main__':
     main()

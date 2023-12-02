6b113e6e637f1fa484c09261204b33adc7c98dcd
papavictor <github@patrickvalencia.com>
2023-12-02 00:27:02 -0500

    puzzle 2

--- 2023/puzzle.py
+++ 2023/puzzle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import math
+
 def puzzle_1_1():
     with open("1.txt") as fp:
         data = fp.read().strip().splitlines()
@@ -50,9 +52,47 @@ def puzzle_1_2():
         total += int(d)
     return total
 
+def puzzle_2_1():
+    with open("2.txt") as fp:
+        data = fp.read().strip().splitlines()
+    allowed = {"red": 12, "green": 13, "blue": 14}
+    count = 0
+    for line in data:
+        (game, games) = line.split(":")
+        game = game.split(" ")[1]
+        for g in games.split(";"):
+            for color in g.split(","):
+                if allowed[color.strip().split(" ")[1]] < int(color.strip().split(" ")[0]):
+                    break
+            else:
+                continue
+            break
+        else:
+            count += int(game)
+    return count
+
+def puzzle_2_2():
+    with open("2.txt") as fp:
+        data = fp.read().strip().splitlines()
+    allowed = {"red": 12, "green": 13, "blue": 14}
+    count = 0
+    for line in data:
+        (game, games) = line.split(":")
+        game = game.split(" ")[1]
+        max_colors = {"red": 0, "green": 0, "blue": 0}
+        for g in games.split(";"):
+            for color in g.split(","):
+                if max_colors[color.strip().split(" ")[1]] < int(color.strip().split(" ")[0]):
+                    max_colors[color.strip().split(" ")[1]] = int(color.strip().split(" ")[0])
+        product = math.prod(max_colors.values())
+        count += product
+    return count
+
 def main():
     print(f"Puzzle 1, part 1: {puzzle_1_1()}")
     print(f"Puzzle 1, part 2: {puzzle_1_2()}")
+    print(f"Puzzle 2, part 1: {puzzle_2_1()}")
+    print(f"Puzzle 2, part 2: {puzzle_2_2()}")
 
 if __name__ == '__main__':
     main()
